933c5c369794c746110a4588841ff43aee177679
JavaGeoGroup <Gog1nator>
2019-08-05 16:23:27 +0400

    secomd commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { AuthGuard } from './_guards/auth.guard';
+import { RegisterComponent } from './register/register.component';
 
 
 const routes: Routes = [
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+
   {path : 'login',  component: LoginComponent},
   {path : 'home',  component: HomeComponent},
+  {path : 'register',  component: RegisterComponent},
   { path: '**', redirectTo: '' }
 ];
 
